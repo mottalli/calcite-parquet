94a59210822b09e0de459fbb0760ec9418a35b07
Marcelo Mottalli <mottalli@yahoo.com>
2016-04-15 15:58:19 -0300

    Changes

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.calcite" % "calcite-core" % "1.7.0"
 libraryDependencies += "ch.qos.logback" %  "logback-classic" % "1.1.6"
+libraryDependencies += "org.slf4j" %  "jul-to-slf4j" % "1.7.0"
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
 libraryDependencies += "org.apache.parquet" % "parquet-column" % "1.8.1"
 libraryDependencies += "org.apache.parquet" % "parquet-hadoop" % "1.8.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,17 +2,26 @@ import java.nio.file.{FileSystems, Files, Path, Paths}
 import java.sql.DriverManager
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.calcite.linq4j.{AbstractEnumerable, Enumerable, Enumerator}
+import org.apache.calcite.adapter.enumerable.EnumerableRel.{Prefer, Result}
+import org.apache.calcite.adapter.enumerable._
+import org.apache.calcite.linq4j._
+import org.apache.calcite.linq4j.tree.{Blocks, Expressions}
+import org.apache.calcite.plan.{RelOptCluster, RelOptTable, RelTraitSet}
+import org.apache.calcite.rel.RelNode
 import org.apache.calcite.{DataContext, rel}
 import org.apache.calcite.rel.`type`.{RelDataType, RelDataTypeFactory}
+import org.apache.calcite.rel.core.TableScan
+import org.apache.calcite.rel.metadata.RelMetadataQuery
 import org.apache.calcite.schema.impl.{AbstractSchema, AbstractTable}
 import org.apache.calcite.schema._
 import org.apache.calcite.sql.`type`.SqlTypeName
 import org.apache.hadoop.fs.{Path => HDFSPath}
+import org.apache.parquet.column.ColumnDescriptor
 import org.apache.parquet.format.converter.ParquetMetadataConverter
 import org.apache.parquet.hadoop.ParquetReader
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName
 import org.apache.parquet.tools.read.{SimpleReadSupport, SimpleRecord}
+import org.slf4j.bridge.SLF4JBridgeHandler
 
 import collection.JavaConversions._
 
@@ -32,7 +41,8 @@ class ParquetSchema(val path: Path) extends AbstractSchema with LazyLogging {
     val idx = basename.lastIndexOf(".")
     val tableName = basename.substring(0, idx).toUpperCase
     logger.debug(s"Creating table '$tableName' from $path")
-    val table = new ParquetTable(path)
+    //val table = new ParquetScannableTable(path)
+    val table = new ParquetTranslatableTable(path)
     tableName -> table
   }
 
@@ -41,14 +51,53 @@ class ParquetSchema(val path: Path) extends AbstractSchema with LazyLogging {
   override protected def getTableMap: java.util.Map[String, Table] = tablesMap
 }
 
-class ParquetTable(path: Path) extends AbstractTable with ScannableTable with LazyLogging {
+class ParquetEnumerator(table: ParquetTable) extends Enumerator[Array[Object]] {
+  val reader = ParquetReader.builder(new SimpleReadSupport, table.hdfsPath).build()
+  var currentRow: Option[SimpleRecord] = None
+
+  override def moveNext(): Boolean = {
+    currentRow = Option(reader.read())
+    currentRow.isDefined
+  }
+
+  val numColumns = table.columns.length
+  val resultRow = collection.mutable.ArrayBuffer.fill[Object](numColumns)(null)
+
+  override def current(): Array[Object] = {
+    val definedValues = currentRow.get.getValues
+
+    for (i <- 0 until numColumns)
+      resultRow(i) = null
+
+    for (nv <- definedValues)
+      resultRow(table.columnsIndexes.get(nv.getName).get) = nv.getValue
+
+    resultRow.toArray
+  }
+
+  override def reset() = {}
+  override def close() = {}
+}
+
+class ParquetEnumerable(table: ParquetTable) extends AbstractEnumerable[Array[Object]] {
+  override def enumerator(): Enumerator[Array[Object]] = new ParquetEnumerator(table)
+
+}
+
+class ParquetTable(path: Path) extends AbstractTable with LazyLogging {
   val hdfsPath = new HDFSPath(path.toString)
 
-  logger.debug(s"Reading Parquet schema from $path")
-  val config = new org.apache.hadoop.conf.Configuration()
-  val metadata = org.apache.parquet.hadoop.ParquetFileReader.readFooter(config, hdfsPath, ParquetMetadataConverter.NO_FILTER)
-  val messageType = metadata.getFileMetaData.getSchema
-  val columns = messageType.getColumns
+  val columns = {
+    val config = new org.apache.hadoop.conf.Configuration()
+    logger.debug(s"Reading Parquet schema from $path")
+    val metadata = org.apache.parquet.hadoop.ParquetFileReader.readFooter(config, hdfsPath, ParquetMetadataConverter.NO_FILTER)
+    val messageType = metadata.getFileMetaData.getSchema
+    messageType.getColumns
+  }
+
+  columns.foreach(c => logger.debug(s"Got column: $c"))
+
+  val columnsIndexes: Map[String, Int] = Map(columns.map(_.getPath()(0)).zipWithIndex.map(x => x._1 -> x._2): _*)
 
   override def getRowType(typeFactory: RelDataTypeFactory): RelDataType = {
     val fieldInfoBuilder = new RelDataTypeFactory.FieldInfoBuilder(typeFactory)
@@ -57,56 +106,93 @@ class ParquetTable(path: Path) extends AbstractTable with ScannableTable with La
       val name = descriptor.getPath.apply(0).toUpperCase
 
       val columnType = descriptor.getType match {
-        case PrimitiveTypeName.FLOAT => typeFactory.createSqlType(SqlTypeName.FLOAT)
-        case PrimitiveTypeName.BINARY => typeFactory.createSqlType(SqlTypeName.BINARY)
-        case PrimitiveTypeName.INT32 => typeFactory.createSqlType(SqlTypeName.INTEGER)
-        case PrimitiveTypeName.INT64 => typeFactory.createSqlType(SqlTypeName.BIGINT)
+        case PrimitiveTypeName.FLOAT => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.FLOAT), true)
+        case PrimitiveTypeName.BINARY => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.VARCHAR), true)
+        case PrimitiveTypeName.INT32 => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.INTEGER), true)
+        case PrimitiveTypeName.INT64 => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BIGINT), true)
         case _ => ???
       }
 
       //logger.debug(s"Field '$name' of type '${descriptor.getType}'")
 
       fieldInfoBuilder.add(name, columnType)
-
     }
 
     fieldInfoBuilder.build()
   }
+}
 
-  override def scan(root: DataContext): Enumerable[Array[Object]] = new AbstractEnumerable[Array[Object]] {
-    override def enumerator(): Enumerator[Array[Object]] = {
-      new Enumerator[Array[Object]] {
+class ParquetScannableTable(path: Path) extends ParquetTable(path) with ScannableTable {
+  override def scan(root: DataContext): Enumerable[Array[Object]] = new ParquetEnumerable(this)
+}
 
-        val reader = ParquetReader.builder(new SimpleReadSupport(), hdfsPath).build()
-        var currentRow: Option[SimpleRecord] = None
+class ParquetTranslatableTable(path: Path) extends ParquetTable(path) with QueryableTable with TranslatableTable {
+  override def asQueryable[T](queryProvider: QueryProvider, schema: SchemaPlus, tableName: String): Queryable[T] = {
+    logger.info("AS QUERYABLE")
+    ???
+  }
 
-        override def current(): Array[Object] = {
-          val values = currentRow.get.getValues
-          currentRow.get.getValues.foreach(nv => println(s"${nv.getName} -> ${nv.getValue}"))
-          println(s"NUMBER OF VALUES: ${values.length}")
-          values.map(_.getValue).toArray
-        }
-        override def reset() = ()
-        override def close() = ()
-        override def moveNext(): Boolean = {
-          currentRow = Option(reader.read())
-          currentRow.isDefined
-        }
-      }
+  override def toRel(context: RelOptTable.ToRelContext, relOptTable: RelOptTable): RelNode = {
+    logger.info("TO REL")
+    //Thread.currentThread().getStackTrace.foreach(println)
+    return new ParquetTableScan(context.getCluster, relOptTable)
+  }
+
+  override def getExpression(schema: SchemaPlus, tableName: String, clazz: java.lang.Class[_]): org.apache.calcite.linq4j.tree.Expression = {
+    logger.info("GET EXPRESSION")
+    Schemas.tableExpression(schema, getElementType(), tableName, clazz)
+  }
+
+  override def getElementType(): java.lang.reflect.Type = {
+    classOf[Array[Object]]
+  }
+
+  def project(): Enumerable[Array[Object]] = {
+    val table = this
+    new AbstractEnumerable[Array[Object]] {
+      override def enumerator(): Enumerator[Array[Object]] = new ParquetEnumerator(table)
     }
   }
 }
 
+class ParquetTableScan(cluster: RelOptCluster, tablex: RelOptTable)
+  extends TableScan(cluster, cluster.traitSetOf(EnumerableConvention.INSTANCE), tablex)
+  with EnumerableRel
+  with LazyLogging {
+
+  override def implement(implementor: EnumerableRelImplementor, pref: Prefer): Result = {
+    logger.info("IMPLEMENT")
+
+    val physType = PhysTypeImpl.of(implementor.getTypeFactory(), getRowType, pref.preferArray())
+    implementor.result(
+      physType,
+      Blocks.toBlock(
+        Expressions.call(table.getExpression(classOf[ParquetTranslatableTable]), "project")
+      )
+    )
+  }
+}
+
 object Main extends App {
+  SLF4JBridgeHandler.removeHandlersForRootLogger()
+  SLF4JBridgeHandler.install()
+
     //new ParquetSchema()
   val jsonPath = getClass.getResource("test-model.json").getPath
   val url = s"jdbc:calcite:model=$jsonPath"
 
   val conn = DriverManager.getConnection(url)
 
-  val rs = conn.createStatement().executeQuery("SELECT SUM(CLIENTS.INCOME) FROM CLIENTS")
+  val rs = conn.createStatement().executeQuery(
+    "SELECT SOCIABILITY_INDEX AS SI, SUM(PERSON_ID) AS TUTTI " +
+    "FROM CLIENTS AS CX " +
+    "WHERE MDN IS NOT NULL AND NUM_TRANSACTIONS > 0" +
+    "GROUP BY SOCIABILITY_INDEX " +
+    "LIMIT 10")
 
-  rs.next()
-  val result = rs.getLong(1)
-  println(s"Result: $result")
+  val numColumns = rs.getMetaData.getColumnCount
+  while (rs.next()) {
+    val row = (1 to numColumns).map(rs.getObject)
+    println(s"Row: $row")
+  }
 }
